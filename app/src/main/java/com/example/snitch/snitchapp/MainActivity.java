a39c34e1065ed95cf93c55f8a5de724493ca0a02
Kirill Sudnikovich <0311snitch@gmail.com>
2018-10-12 00:21:31 +0300

    initial commit

---
+++ app/src/main/java/com/example/snitch/snitchapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.snitch.snitchapp;
+
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    static final int PERMISSION_READ_STATE = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int permissonCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+        if (permissonCheck == PackageManager.PERMISSION_GRANTED) {
+            MyTelephonyManager();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE},
+                    PERMISSION_READ_STATE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_READ_STATE:{
+                if (grantResults.length >= 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    MyTelephonyManager();
+                } else {
+                    Toast.makeText(this, "You don't have required permission to make this action", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private void MyTelephonyManager() {
+        TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = manager.getDeviceId();
+        TextView varText = (TextView)findViewById(R.id.bob);
+        varText.setText(imei);
+    }
+
+}
