684724900439fb49ac868d55d3682b2cb61954a8
Kirill Sudnikovich <0311snitch@gmail.com>
2018-12-18 12:55:29 +0300

    [add] hide bottom navigation before user auth

--- app/src/main/java/com/example/snitch/snitchapp/MainActivity.java
+++ app/src/main/java/com/example/snitch/snitchapp/MainActivity.java
@@ -18,27 +18,52 @@ import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity  implements BlankFragment.OnFragmentInteractionListener, BlankFragment2.OnFragmentInteractionListener, BlankFragment3.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener, BlankFragment.OnFragmentInteractionListener, BlankFragment2.OnFragmentInteractionListener, BlankFragment3.OnFragmentInteractionListener {
     NavController navController;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
+    private EditText ETemail;
+    private EditText ETpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        setuoBottomNavMenu();
+        setupBottomNavMenu();
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
     }
 
-    public void setuoBottomNavMenu() {
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        ETemail = (EditText) findViewById(R.id.email_field);
+        ETpassword = (EditText) findViewById(R.id.password_field);
+        findViewById(R.id.signup_button).setOnClickListener(this);
+        findViewById(R.id.signin_button).setOnClickListener(this);
+//        updateUI(currentUser);
+    }
+
+    public void setupBottomNavMenu() {
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setVisibility(View.GONE);
         NavigationUI.setupWithNavController(bottomNav, navController);
     }
 
@@ -68,5 +93,56 @@ public class MainActivity extends AppCompatActivity  implements BlankFragment.On
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.signup_button) {
+            SignUp(ETemail.getText().toString(), ETpassword.getText().toString());
+        }
+        else if (view.getId() == R.id.signin_button) {
+            SignIn(ETemail.getText().toString(), ETpassword.getText().toString());
+        }
+    }
+
+    public void SignUp(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Регистрация успешна", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Регистрация провалена", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void SignIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Авторизация успешна", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+                            bottomNav.setVisibility(View.VISIBLE);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Авторизация провалена", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
